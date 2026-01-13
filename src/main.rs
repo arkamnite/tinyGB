94015c13bfdf6b209bafe08b161d1e8c2e1a8d17
Akram Ahmad <bulgeypip786@gmail.com>
2026-01-13 19:55:04 +0000

    main: change test source string, tidy
    Simplify the test string to include an entry and data section for ROM
    bank 0, with some example instructions. Remove unnecessary code from the
    main program.

--- src/main.rs
+++ src/main.rs
@@ -12,29 +12,17 @@ mod rom_builder;
 use parser::Parser;
 
 fn main() {
-    // let input = "
-    //     .section .data
-    // str:    .asciz \"Hello, world!\"
-
-    //     .section .text
-    //     .global _start
-
-    // _start:
-    //     ld %a, #5
-    // ";
     let input = "
     .section .entry
         nop
         jp $0150
 
     .section .data 0
-        ld %a, %c
         ld %a, $c
         ld $c, %a
-        ";
-    // ld %a, #5
-    // let _tokens = lexer::tokenize(input);
-    println!("==================== Parser ====================");
+        ld %a, %c
+    ";
+
     let mut lexer = Token::lexer(input);
     let mut parser = Parser::new();
     let _instr = parser.parse(&mut lexer).unwrap();
@@ -43,6 +31,4 @@ fn main() {
     let rom = builder.build_rom();
 
     fs::write("test.gb", rom.data.as_slice()).ok();
-
-    println!("==================== /Parser ===================");
 }
